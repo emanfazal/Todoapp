d99ee1485436e921a237ce4e3f0244b710a26f89
emanfazal <emaanfazal52@gmail.com>
2024-09-03 16:38:35 +0500

    the changes updated

---
+++ lib/AppComponents/TaskDialog.dart
@@ -0,0 +1,253 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../Providers/TodoProvider.dart';
+import '../../../model/taskmodel.dart';
+import '../Constants/utils.dart'; // Ensure this imports AppColors
+
+class TaskDialogHelper {
+  // Method to show Add Task Dialog
+  static void showAddTaskDialog(BuildContext context) {
+    final TextEditingController taskController = TextEditingController();
+    DateTime? selectedDueDate;
+    TimeOfDay? selectedDueTime;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+              primary: AppColors.primaryColor, // Apply your primary color
+            ),
+            buttonTheme: ButtonThemeData(
+              textTheme: ButtonTextTheme.primary,
+            ),
+          ),
+          child: AlertDialog(
+            title: Text('Add New Task'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: taskController,
+                  decoration: InputDecoration(labelText: 'Task'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text(selectedDueDate == null
+                      ? 'Select Due Date'
+                      : 'Due Date: ${selectedDueDate?.toLocal()}'.split(' ')[0]),
+                  onPressed: () async {
+                    final selectedDate = await _selectDueDate(context);
+                    if (selectedDate != null) {
+                      selectedDueDate = selectedDate;
+                      // To refresh the button text
+                      (context as Element).markNeedsBuild();
+                    }
+                  },
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  child: Text(selectedDueTime == null
+                      ? 'Select Due Time'
+                      : 'Due Time: ${selectedDueTime!.format(context)}'),
+                  onPressed: () async {
+                    final selectedTime = await _selectDueTime(context);
+                    if (selectedTime != null) {
+                      selectedDueTime = selectedTime;
+                      // To refresh the button text
+                      (context as Element).markNeedsBuild();
+                    }
+                  },
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              TextButton(
+                child: Text('Add'),
+                onPressed: () {
+                  final task = taskController.text;
+                  if (task.isNotEmpty && selectedDueDate != null && selectedDueTime != null) {
+                    final dueDateTime = DateTime(
+                      selectedDueDate!.year,
+                      selectedDueDate!.month,
+                      selectedDueDate!.day,
+                      selectedDueTime!.hour,
+                      selectedDueTime!.minute,
+                    );
+                    final newTodo = Todo(
+                      id: DateTime.now().millisecondsSinceEpoch.toString(), // Use a unique ID
+                      task: task,
+                      isDone: false,
+                      CreatedOn: Timestamp.now(),
+                      UpdatedOn: Timestamp.now(),
+                      dueDateTime: Timestamp.fromDate(dueDateTime),
+                    );
+                    Provider.of<TodoProvider>(context, listen: false).addTodo(newTodo);
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Method to show Edit Task Dialog
+  static void showEditTaskDialog(BuildContext context, Todo todo) {
+    final TextEditingController taskController = TextEditingController(text: todo.task);
+    DateTime? selectedDueDate = todo.dueDateTime?.toDate();
+    TimeOfDay? selectedDueTime = selectedDueDate != null ? TimeOfDay.fromDateTime(selectedDueDate) : null;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+              primary: AppColors.primaryColor, // Apply your primary color
+            ),
+            buttonTheme: ButtonThemeData(
+              textTheme: ButtonTextTheme.primary,
+            ),
+          ),
+          child: AlertDialog(
+            title: Text('Edit Task'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: taskController,
+                  decoration: InputDecoration(labelText: 'Task'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text(selectedDueDate == null
+                      ? 'Select Due Date'
+                      : 'Due Date: ${selectedDueDate?.toLocal()}'.split(' ')[0]),
+                  onPressed: () async {
+                    final pickedDate = await _selectDueDate(context);
+                    if (pickedDate != null) {
+                      selectedDueDate = pickedDate;
+                      // To refresh the button text
+                      (context as Element).markNeedsBuild();
+                    }
+                  },
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  child: Text(selectedDueTime == null
+                      ? 'Select Due Time'
+                      : 'Due Time: ${selectedDueTime!.format(context)}'),
+                  onPressed: () async {
+                    final pickedTime = await _selectDueTime(context);
+                    if (pickedTime != null) {
+                      selectedDueTime = pickedTime;
+                      // To refresh the button text
+                      (context as Element).markNeedsBuild();
+                    }
+                  },
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              TextButton(
+                child: Text('Update'),
+                onPressed: () {
+                  final updatedTask = taskController.text;
+                  if (updatedTask.isNotEmpty && selectedDueDate != null && selectedDueTime != null) {
+                    final dueDateTime = DateTime(
+                      selectedDueDate!.year,
+                      selectedDueDate!.month,
+                      selectedDueDate!.day,
+                      selectedDueTime!.hour,
+                      selectedDueTime!.minute,
+                    );
+                    final updatedTodo = todo.copyWith(
+                      task: updatedTask,
+                      UpdatedOn: Timestamp.now(),
+                      dueDateTime: Timestamp.fromDate(dueDateTime),
+                    );
+                    Provider.of<TodoProvider>(context, listen: false).updateTodo(updatedTodo);
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Method to select Due Date with Theme
+  static Future<DateTime?> _selectDueDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+              primary: AppColors.primaryColor, // Apply your primary color
+              onPrimary: Colors.white, // Text color on primary
+              onSurface: Colors.black, // Text color on surface
+            ),
+            dialogBackgroundColor: Colors.white,
+          ),
+          child: child!,
+        );
+      },
+    );
+    return pickedDate;
+  }
+
+  // Method to select Due Time with Theme
+  static Future<TimeOfDay?> _selectDueTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+              primary: AppColors.primaryColor, // Apply your primary color
+              onPrimary: Colors.white, // Text color on primary
+              onSurface: Colors.black, // Text color on surface
+            ),
+            dialogBackgroundColor: Colors.white,
+          ),
+          child: child!,
+        );
+      },
+    );
+    return pickedTime;
+  }
+
+  // Method to delete a task
+  static void deleteTask(BuildContext context, String id) {
+    Provider.of<TodoProvider>(context, listen: false).deleteTodo(id);
+  }
+
+  // Method to toggle task completion
+  static void toggleTaskCompletion(BuildContext context, Todo todo) {
+    final updatedTodo = todo.copyWith(
+      isDone: !todo.isDone,
+      UpdatedOn: Timestamp.now(),
+    );
+    Provider.of<TodoProvider>(context, listen: false).updateTodo(updatedTodo);
+  }
+}
--- lib/view/TaskScreen/Components/TaskBody.dart
+++ lib/view/TaskScreen/Components/TaskBody.dart
@@ -1,88 +1,55 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../AppComponents/CustomAppbar.dart';
+import '../../../AppComponents/TaskDialog.dart';
+import '../../../Constants/utils.dart';
 import '../../../Providers/TodoProvider.dart';
-import '../../../model/taskmodel.dart'; // Ensure the Todo model is imported
+import '../../../model/taskmodel.dart';
+// Import the helper class
 
 class TaskBody extends StatelessWidget {
   const TaskBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Custom AppBar if you have one
-      body: Consumer<TodoProvider>(
-        builder: (context, todoProvider, child) {
-          if (todoProvider.todos.isEmpty) {
-            return Center(child: Text('No tasks found.'));
-          } else {
-            return Column(
-              children: [
-                CustomAppBar(),
-                ListView.builder(
-                  itemCount: todoProvider.todos.length,
-                  itemBuilder: (context, index) {
-                    final todo = todoProvider.todos[index];
-                    return ListTile(
-                      title: Text(todo.task),
-                      subtitle: Text('Completed: ${todo.isDone ? "Yes" : "No"}'),
-                      trailing: Text('ID: ${todo.id}'),
-                    );
-                  },
+    return Consumer<TodoProvider>(
+      builder: (context, todoProvider, child) {
+        if (todoProvider.todos.isEmpty) {
+          return Center(child: Text('No tasks found.'));
+        } else {
+          return ListView.builder(
+            itemCount: todoProvider.todos.length,
+            itemBuilder: (context, index) {
+              final todo = todoProvider.todos[index];
+              return Card(
+                surfaceTintColor: AppColors.primaryColor,
+                shadowColor: AppColors.Gradient1,
+                child: ListTile(
+                  title: Text(todo.task),
+                  subtitle: Text(
+                    'Completed: ${todo.isDone ? "Yes" : "No"}\n' +
+                        (todo.dueDateTime != null
+                            ? 'Due: ${todo.dueDateTime!.toDate().toLocal()}'
+                            : 'No Due Date'),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () => TaskDialogHelper.showEditTaskDialog(context, todo),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => TaskDialogHelper.deleteTask(context, todo.id),
+                      ),
+                    ],
+                  ),
+                  onTap: () => TaskDialogHelper.toggleTaskCompletion(context, todo),
                 ),
-              ],
-            );
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showAddTaskDialog(context),
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _showAddTaskDialog(BuildContext context) {
-    final TextEditingController taskController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Add New Task'),
-          content: TextField(
-            controller: taskController,
-            decoration: InputDecoration(hintText: 'Enter task description'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                final task = taskController.text.trim();
-                if (task.isNotEmpty) {
-                  final newTodo = Todo(
-                    CreatedOn: Timestamp.now(), // Default value
-                    UpdatedOn: Timestamp.now(),
-                    id: DateTime.now().toString(), // Generate a unique ID
-                    task: task,
-                    isDone: false,
-                    // Default value
-                  );
-
-                  Provider.of<TodoProvider>(context, listen: false).addTodo(newTodo);
-                  Navigator.of(context).pop();
-                }
-              },
-              child: Text('Add'),
-            ),
-          ],
-        );
+              );
+            },
+          );
+        }
       },
     );
   }
